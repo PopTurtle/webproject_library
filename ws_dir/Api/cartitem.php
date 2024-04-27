529ab4c7e393e75fe88b225bf5c7de4aae1d52ec
PopTurtle <kilian.map@gmail.com>
2024-04-27 15:51:18 +0200

    Modification de l'API

--- ws_dir/Api/cartitem.php
+++ ws_dir/Api/cartitem.php
@@ -5,19 +5,27 @@ use App\Model\DBObjects\CartItem;
 
 require_once "../autoloader.php";
 
-// Pour le moment : si action == "add" alors ajoute le livre book_id au panier
-$res = ["status" => -1];
+$options = $_POST;
 
-if (isset($_POST["action"]) && strcmp($_POST["action"], "add") == 0) {
-    if (!isset($_POST["book_id"])) {
-        $res["status"] = -2;
-    } else {
-        $f = CartItem::tryAddToShoppingCart(
-            intval($_POST["book_id"]),
-            SessionManager::Instance()->getUserConsumer()->Id
-        );
-        $res["status"] = $f === false ? -3 : 0;
+function actionAdd($options) {
+    if (!isset($options["book_id"])) {
+        return "no content";
     }
+    $c = SessionManager::Instance()->getUserConsumer();
+    if ($c === null) {
+        return "no user";
+    }
+    $f = CartItem::tryAddToShoppingCart(intval($options["book_id"]), $c->Id);
+    return $f === false ? "no content" : "ok";
+}
+
+
+
+// Pour le moment : si action == "add" alors ajoute le livre book_id au panier
+$res = ["status" => "error"];
+
+if (isset($options["action"]) && strcmp($options["action"], "add") == 0) {
+    $res["status"] = actionAdd($options);
 }
 
 header("Content-Type: application/json");
