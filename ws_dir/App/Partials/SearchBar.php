e47dc280d0cb5b8a58df4f4a2878143ea9c0bf76
PopTurtle <kilian.map@gmail.com>
2024-04-29 20:22:39 +0200

    Styling searchbar

--- ws_dir/App/Partials/SearchBar.php
+++ ws_dir/App/Partials/SearchBar.php
@@ -19,9 +19,9 @@ class SearchBar extends Partial {
             <form method="get" action="<?= $args['action_ref'] ?? '' ?>">
                 <input class="sb-text" type="text" name="search-data" placeholder="Rechercher un livre">
                 <select class="sb-drop" name="search-type">
-                    <option value="<?= Constants::SEARCH_TYPE_TITLE ?>">Titre</option>
-                    <option value="<?= Constants::SEARCH_TYPE_AUTHOR ?>">Auteur</option>
-                    <option value="<?= Constants::SEARCH_TYPE_CATEGORY ?>">Catégorie</option>
+                    <option value="<?= Constants::SEARCH_TYPE_TITLE ?>">Par titre</option>
+                    <option value="<?= Constants::SEARCH_TYPE_AUTHOR ?>">Par auteur</option>
+                    <option value="<?= Constants::SEARCH_TYPE_CATEGORY ?>">Par catégorie</option>
                 </select>
                 <input class="sb-search" type="submit" value="Rechercher">
             </form>
