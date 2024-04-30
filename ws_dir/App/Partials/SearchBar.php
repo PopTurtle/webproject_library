311dd7b4201d161c50b608d94017c02d7490819d
PopTurtle <kilian.map@gmail.com>
2024-04-30 23:22:53 +0200

    Polissage CSS (animations)

--- ws_dir/App/Partials/SearchBar.php
+++ ws_dir/App/Partials/SearchBar.php
@@ -17,7 +17,13 @@ class SearchBar extends Partial {
         ?>
         <div class="search-bar">
             <form method="get" action="<?= $args['action_ref'] ?? '' ?>">
-                <input class="sb-text" type="text" name="search-data" placeholder="Rechercher un livre">
+                <input
+                    class="sb-text"
+                    type="text"
+                    name="search-data"
+                    placeholder="Rechercher un livre"
+                    autocomplete="off"
+                    >
                 <select class="sb-drop" name="search-type">
                     <option value="<?= Constants::SEARCH_TYPE_TITLE ?>">Par titre</option>
                     <option value="<?= Constants::SEARCH_TYPE_AUTHOR ?>">Par auteur</option>
