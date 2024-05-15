19461d7160ea28772eeb9a8abc6547abb6d063e4
PopTurtle <kilian.map@gmail.com>
2024-05-15 21:53:18 +0200

    Modification du deploy.sh : ajout d'un chmod pour le volume des couvertures

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,10 @@ fi
 
 docker image rm webproject_library
 docker compose up -d
+
+#  Changer le nom de l'image si docker-compose est modifié (Format: name_volume)
+#  Ici: name = webproject, volume = ws_storage , donc webproject_ws_storage
+storage_path=$(docker volume inspect --format '{{ .Mountpoint }}' webproject_ws_storage)
+echo "Found storage path: $storage_path"
+echo "Ajout de la permission d'écrire dans le dossier des couvertures"
+sudo chmod 777 "$storage_path/Cover"
