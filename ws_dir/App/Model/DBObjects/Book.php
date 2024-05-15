a314a9117a2410f63004aa258c9f7c5ab9d37ff1
PopTurtle <kilian.map@gmail.com>
2024-05-15 20:57:20 +0200

    Correction de bug

--- ws_dir/App/Model/DBObjects/Book.php
+++ ws_dir/App/Model/DBObjects/Book.php
@@ -40,7 +40,7 @@ class Book extends DBObject {
         "Title", "Author", "Editor", "PublicationYear", "Category", "Stock"
     ];
 
-    public const CoverPlaceholderImage = "/App/Assets/Images/Logo.png";
+    public const CoverPlaceholderImage = "/App/Assets/Images/logo.png";
 
     /**
      *  Renvoie tous les livres dont le champs $type (title, author...) contient
@@ -85,9 +85,12 @@ class Book extends DBObject {
 
     public static function setBookCover(int $bookId, string $filepath): bool {
         $rpath = Utils::getFullStorageFolder() . "/Cover/cov_" . strval($bookId) . ".jpg";
+        var_dump($rpath);
         if (!move_uploaded_file($filepath, $rpath)) {
+            echo "move_uploaded_file FAILED <br>";
             return false;
         }
+        echo "move_uploaded_file SUCCESS <br>";
         return true;
     }
 
@@ -120,6 +123,9 @@ class Book extends DBObject {
     }
 
     private static function treatFormSetCover(?int $bookId = null): int {
+        echo "SETTING COVER <br>";
+        var_dump($bookId);
+        var_dump($_FILES);
         //  Ajoute la couverture si elle existe
         //  ! Ne renvoie pas d'erreur en cas d'échec
         if (!isset($_FILES[static::FormCoverArg])) {
@@ -133,6 +139,8 @@ class Book extends DBObject {
             $bookId = $q->fetch()[0];
         }
         $path = $_FILES[static::FormCoverArg]["tmp_name"];
+        var_dump($bookId);
+        var_dump($path);
         self::setBookCover($bookId, $path);
         return 0;
     }
