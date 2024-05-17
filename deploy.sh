ae66e55d4c6639a3587dcce29defe4675a584645
PopTurtle <kilian.map@gmail.com>
2024-05-17 16:51:54 +0200

    Correction et changement de KEEP_VOLUMES lors du déploiement

--- deploy.sh
+++ deploy.sh
@@ -46,7 +46,7 @@ echo "------------------------------"
 docker image rm webproject_library
 docker compose up -d
 
-#  Changer le nom de l'image si docker-compose est modifié (Format: name_volume)
+#  Changer le nom du volume si docker-compose est modifié (Format: name_volume)
 #  Ici: name = webproject, volume = ws_storage , donc webproject_ws_storage
 storage_path=$(docker volume inspect --format '{{ .Mountpoint }}' webproject_ws_storage)
 echo "Found storage path: $storage_path"
